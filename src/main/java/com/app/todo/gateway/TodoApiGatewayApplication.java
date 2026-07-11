867deddfc7f3765bfd028bcf8a566f57cae9b202
Kiran Ganji <kiranganji2003@gmail.com>
2026-07-11 16:38:28 +0530

    gateway added

---
+++ src/main/java/com/app/todo/gateway/TodoApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.app.todo.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/todo/gateway/TodoApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.todo.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
